18280e6831a318ec3ff8aed4bf7dce8cfebfe225
hongwei1 <hongwei@tesobe.com>
2018-09-16 09:05:59 +0200

    set up intellij ignore

---
+++ T2-Basics.py
@@ -0,0 +1,8 @@
+print('hello world') # Note that print is a function
+
+# Code tells you how, comments should tell you why.
+# explain assumptions
+# explain important decisions
+# explain important details
+# explain problems you're trying to solve
+# explain problems you're trying to overcome in your program, etc.
