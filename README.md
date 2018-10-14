cf3e7412bb19518e6a429b2080570e0f2f69c96f
hongwei1 <hongwei@tesobe.com>
2018-10-14 22:26:25 +0200

    added the book info - byte_of_python-Eg

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # LearnPython
 Just for myself
+
+### byte_of_python-Eg package is for 
+
+https://python.swaroopch.com/
--- byte_of_python-Eg/Chapter 5. Basics.py
+++ byte_of_python-Eg/Chapter 5. Basics.py
@@ -1,5 +1,5 @@
 print('hello world') # Note that print is a function
-
+# byte_of_python-Eg
 # Code tells you how, comments should tell you why.
 # explain assumptions
 # explain important decisions
